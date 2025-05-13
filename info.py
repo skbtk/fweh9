bcb88b8a26bffd976039bf96edbe9dc6ff3b630e
skbtk <officialilmsphere@gmail.com>
2025-05-13 11:59:47 +0530

    Delete info.py

--- info.py
+++
@@ -1,7 +0,0 @@
-import os
-
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID"))
-UPLOAD_CHANNEL = int(os.getenv("UPLOAD_CHANNEL"))
